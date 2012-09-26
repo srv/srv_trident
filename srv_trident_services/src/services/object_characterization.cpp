f68d09e99586f2f5cded540b036484c87126b7df
Stephan Wirth <stephan.wirth@uib.es>
2012-09-26 08:47:01 +0200

    working object characterization service

--- srv_trident_services/src/services/object_characterization.cpp
+++ srv_trident_services/src/services/object_characterization.cpp
@@ -1,7 +1,7 @@
-#include <actionlib/server/simple_action_server.h>
 
 #include <matchmaker/client.h>
-#include <auv_msgs/CharacterizeObjectAction.h>
+
+#include <vision_msgs/TrainDetector.h>
 
 namespace srv_trident_services
 {
@@ -11,51 +11,41 @@ class ObjectCharacterization
 
 public:
 
-  ObjectCharacterization()
+  ObjectCharacterization() : nh_priv_("~")
   {
-    std::string action_name;
-    nh_.param<std::string>("action_name", action_name, "characterize_object");
+    std::string soa_service_name;
+    nh_priv_.param<std::string>("soa_service_name", soa_service_name, "Object Characterization");
 
-    // create the action server
-    bool auto_start(false);
-    action_server_.reset(
-        new actionlib::SimpleActionServer<auv_msgs::CharacterizeObjectAction>(
-          nh_, action_name, 
-          boost::bind(&ObjectCharacterization::actionCallback, this, _1), 
-          auto_start));
+    std::string topic = nh_.resolveName("train");
+    mm_service_ad_ = mm_client_.advertiseService(soa_service_name, topic);
 
-    // advertize the service
-    mm_service_ad_ = mm_client_.advertiseService("Object Characterization", "characterize_object");
+    ROS_INFO("Looking for ROS service on topic %s", topic.c_str());
+    service_client_ = nh_.serviceClient<vision_msgs::TrainDetector>("train");
 
-    // connect to training action server and image server
-    // on success, set
-    mm_service_ad_.setStatus(matchmaker::ServiceStatus::AVAILABLE);
-    ROS_INFO_STREAM("Service \"Object Characterization\" is now available.");
+    timer_ = nh_.createTimer(ros::Duration(1.0), &ObjectCharacterization::checkService, this);
   }
 
-  void actionCallback(const auv_msgs::CharacterizeObjectGoalConstPtr& goal)
+  void checkService(const ros::TimerEvent&)
   {
-    // unpack goal
-
-    // perform training and give feedback
-    // 1. retrieve images
-    // 2. create masks
-    // 3. send training data
-
-    // return training result
-
-    auv_msgs::CharacterizeObjectResult result;
-    action_server_->setSucceeded(result);
+    if (!service_client_.exists())
+    {
+      ROS_INFO("Waiting for ROS service...");
+      mm_service_ad_.setStatus(matchmaker::ServiceStatus::REMOVED);
+    }
+    else
+    {
+      mm_service_ad_.setStatus(matchmaker::ServiceStatus::AVAILABLE);
+      ROS_INFO("Service \"Object Characterization\" is now available.");
+    }
   }
-
 
 private:
   ros::NodeHandle nh_;
+  ros::NodeHandle nh_priv_;
+  ros::ServiceClient service_client_;
   matchmaker::Client mm_client_;
   matchmaker::ServiceAdvertiser mm_service_ad_;
-
-  boost::shared_ptr<actionlib::SimpleActionServer<auv_msgs::CharacterizeObjectAction> > action_server_;
-
+  ros::Timer timer_;
 };
 
 
